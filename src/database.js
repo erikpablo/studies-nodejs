9710609ae7b87dd1c85ae892580b8c0410e08b35
Erik Pablo <nunespablo470@gmail.com>
2025-05-23 09:58:08 -0300

    database

--- src/database.js
+++ src/database.js
@@ -1,27 +1,54 @@
+import fs from 'node:fs/promises'  // Importando o módulo fs do Node.js para manipulação de arquivos
+
 /**
- * Banco de dados 
- * -Usado para armazenar os dados do usuário
+ * persist() - ira escrver os dados no arquivo fisico
+ * -- dentro 
+ * ---fs.writeFile - escreve os dados no arquivo
  * 
- * criamos o datatbase = {}
+ * ira ser chamado sempre que houver uma alteração no banco de dados
+ * --sempre que inserir 
  * 
- * select() {}
- * - usado para selecionar os dados
+ * importa meta url ira retorna o caminho do arquivo
  * 
- * insert() {}
- * -usado para a insersão de dados
+ * class interna do node chamado url / costrutora
  * 
- * LEMBRA QUE [NAME] TAMBEM ACESSAMOS DADOS DE OBJETO
+ * new URL(node do arquivo do final, import.meta.url/local)
+ * 
+ * agora queremos que os arquvios do banco sejam salvos
+ * 
+ * -- criamos um constrututor 
+ * ---- criamos o readFile recebdo o databasePath para ser 
+ * e usando o then pegamos os dados e salvamos dentro do database
+ * ---usando o catch podemos pegar o dado e passar mesmo estando vazio 
+ * 
  */
+
+const databasePath = new URL('../db.json', import.meta.url) // Importando o caminho do arquivo db.json
+
 
 export class Database {
     #database = {}
 
-    select(table) {
-        const data = this.#database[table] ?? []
-
-        return data
+    constructor() {
+        fs.readFile(databasePath, 'utf-8')
+            .then(data => {
+                this.#database = JSON.parse(data)
+            })
+            .catch(() => {
+                this.#persist()
+            })
     }
 
+    #persist() {
+        fs.writeFile(databasePath, JSON.stringify(this.#database))
+    }
+    
+    select(table) {
+        const data = this.#database[table] ?? []
+        
+        return data
+    }
+    
     insert(table, data) {
         if(Array.isArray(this.#database[table])) {
             this.#database[table].push(data)
@@ -29,6 +56,25 @@ export class Database {
             this.#database[table] = [data]
         }
 
+        this.#persist()
+        
         return data
     }
 }
+
+
+
+    /**
+     * Banco de dados 
+     * -Usado para armazenar os dados do usuário
+     * 
+     * criamos o datatbase = {}
+     * 
+     * select() {}
+     * - usado para selecionar os dados
+     * 
+     * insert() {}
+     * -usado para a insersão de dados
+     * 
+     * LEMBRA QUE [NAME] TAMBEM ACESSAMOS DADOS DE OBJETO
+     */
